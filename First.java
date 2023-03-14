436fbf11b9099bd3d73b3639c90d82e2472c99ce
juuliasuvorova <127865714+juuliasuvorova@users.noreply.github.com>
2023-03-14 22:28:58 +0800

    Update First.java

--- First.java
+++ First.java
@@ -1,11 +1,11 @@
 package first;
 public class First {
     public static void main(String[] args) {
-         for (int i = 1; i <= 100; i++) { /*перебор числел от 1 до 100*/
-            if (i % 3 == 0 && i % 5 == 0) { /*проверка кратности числа на 3 и 5*/
-                System.out.println("FizzBuzz")}else if (i % 3 == 0 ) { /*проверка кратности числа на 3*/
+         for (int i = 1; i <= 100; i++) { /* перебираем числа от 1 до 100 */
+            if (i % 3 == 0 && i % 5 == 0) { /*проверяем кратность числа на 3 и 5*/
+                System.out.println("FizzBuzz")}else if (i % 3 == 0 ) { /*проверяем кратность числа на 3*/
                 System.out.println("Fizz");
-            }else if (i % 5 == 0) { /*проверка кратности числа на 5*/
+            }else if (i % 5 == 0) { /*проверяем кратность числа на 5*/
                 System.out.println("Buzz");
             }else {
                 System.out.println(i);
